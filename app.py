84f5c9b94f1b0c967afc9aae83fda847c280369f
Qiusheng Wu <giswqs@gmail.com>
2020-11-02 21:46:51 -0500

    Clean up code

--- app.py
+++ app.py
@@ -2,18 +2,12 @@ import streamlit as st
 from streamlit_folium import folium_static
 import folium
 
-# st.beta_set_page_config(page_title="streamlit-folium documentation")
-
-"# streamlit-folium"
+"# streamlit-geemap"
 
 with st.echo():
     import streamlit as st
     from streamlit_folium import folium_static
     import ee
-    # import folium
-
-    # ee.Initialize()
-
     import geemap.eefolium as geemap
 
     m = geemap.Map()
@@ -26,17 +20,7 @@ with st.echo():
     'palette': ['006633', 'E5FFCC', '662A00', 'D8D8D8', 'F5F5F5']}
 
     m.addLayer(dem, vis_params, 'DEM')
-
     m.addLayerControl()
-
-    # # center on Liberty Bell
-    # m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
-
-    # # add marker for Liberty Bell
-    # tooltip = "Liberty Bell"
-    # folium.Marker(
-    #     [39.949610, -75.150282], popup="Liberty Bell", tooltip=tooltip
-    # ).add_to(m)
 
     # call to render Folium map in Streamlit
     folium_static(m)
