0f1372b8c34625725c15080f140bbae788b83e2e
Qiusheng Wu <giswqs@gmail.com>
2020-11-02 21:41:57 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
  
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"giswqs@gmail.com\"\n\
+echo "[general]
+email = \"giswqs@gmail.com\"
 " > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
